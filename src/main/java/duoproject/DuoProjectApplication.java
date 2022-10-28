7d7e0d81b5c1811e5bc4ecdd83b9c2dba2d5bc44
roal <replicaset01@github.com>
2022-10-28 22:11:38 +0900

    v1.Initial commit

---
+++ src/main/java/duoproject/DuoProjectApplication.java
@@ -0,0 +1,13 @@
+package duoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoProjectApplication.class, args);
+	}
+
+}
