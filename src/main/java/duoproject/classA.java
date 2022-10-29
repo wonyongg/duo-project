f95bb2960e07423e4ba459a87265e46c083d5df4
wonyongg <kakaohwy@gmail.com>
2022-10-29 18:03:25 +0900

    test

---
+++ src/main/java/duoproject/classA.java
@@ -0,0 +1,7 @@
+package duoproject;
+
+public class classA {
+    public static void main(String[] args) {
+        System.out.println("testA");
+    }
+}
---
+++ src/main/java/duoproject/classB.java
@@ -0,0 +1,7 @@
+package duoproject;
+
+public class classB {
+    public static void main(String[] args) {
+        System.out.println("testB");
+    }
+}
