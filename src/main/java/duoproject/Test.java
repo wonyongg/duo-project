54a62afab87d0d2ab642fa52289d1812bb455b93
roal <replicaset01@github.com>
2022-10-29 18:09:07 +0900

    test2

--- src/main/java/duoproject/Test.java
+++ src/main/java/duoproject/Test.java
@@ -1,8 +1,7 @@
 package duoproject;
 
-public class classB {
+public class Test {
     public static void main(String[] args) {
-        System.out.println("testB");
+        System.out.println("abc");
     }
-
 }
